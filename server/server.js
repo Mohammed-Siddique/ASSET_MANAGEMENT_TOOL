f23b7458986db457fc32cd8a1b18d451b7ab7245
Mohammed Siddique <khanshahid9066@gmail.com>
2025-12-10 22:20:31 +0530

    AMT
    Creating and establishing database connection to the backend server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 import express from 'express'
 const port = process.env.PORt ||8000;
+import path from 'path';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import connectDB from './config/db.js';
+connectDB();
+
 
 const app = express();
 
