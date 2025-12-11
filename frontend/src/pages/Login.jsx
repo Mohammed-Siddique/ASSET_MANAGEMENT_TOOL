a58847de2a20ba7510712579a116aa56b7743ce7
Mohammed Siddique <khanshahid9066@gmail.com>
2025-12-11 20:15:38 +0530

    AMT
    Creating Login Page in frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div className='login-container'>
+      <h2>ASSET MANAGEMENT SYSTEM</h2>
+      <form action="">
+        <h2>LOGIN</h2>
+
+        <div>
+          <label htmlFor="">USERNAME</label>
+          <input type="text" required placeholder='Enter Your Name' />
+        </div>
+
+        <div>
+          <label htmlFor="">PASSWORD</label>
+          <input type="password" required placeholder='Enter Your Password' />
+        </div>
+
+        <div className='check-box-frgtpw'>
+          <div className='check-box'>
+            <input type="checkbox" />
+            <label htmlFor="">Remember Me</label>
+          </div>
+          <div className='frgtpw'>
+            <a href="#">Forgot Password?</a>
+          </div>
+        </div>
+
+        <button>LOGIN</button>
+
+      </form>
+    </div>
+    </>
   )
 }
 
